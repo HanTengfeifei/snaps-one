62f2fbd0b1f068a2c83aaf4bd849686d3c75e7b8
tengfeihan <1157123521@qq.com>
2022-08-29 11:59:45 +0800

    init

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
-// import * as Web3MQ from 'mq-web3'
+import * as Web3MQ from 'mq-web3'
 export const onRpcRequest = async ({ origin, request }) => {
   switch (request.method) {
     case 'web3-mq':
       return new Promise((resolve,reject)=>{
         resolve(
-          'Web3MQ'
+          {
+            Web3MQ:Web3MQ,
+            name:"test"
+          }
           )
       })
       
